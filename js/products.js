f53bead3a1dd4a889ebdedb027f4421a9decbc3e
Nikita Fedorov <nikitafedorov666@icloud.com>
2021-10-30 00:41:51 +0300

    update modal

--- js/products.js
+++ js/products.js
@@ -5,7 +5,11 @@ let dataLength = null
 const modal = document.querySelector('.modal')
 const modalOverlay = document.querySelector('.modal__overlay')
 const modalContent = document.querySelector('.modal__content')
-const prodModal = document.querySelector('.modal__content')
+const prodModal = document.querySelector('.product-modal')
+const prodModalSlider = prodModal.querySelector('.product-slider__wrapper')
+const prodModalInfo = prodModal.querySelector('.modal-info__wrapper')
+const prodModalDescr = prodModal.querySelector('.modal-prod-descr')
+const prodModalChars = prodModal.querySelector('.prod-chars')
 
 const normalPrice = (str) => {
 	return String(str).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
@@ -17,20 +21,85 @@ const loadModalData = (id = 1) => {
         return response.json()
     })
     .then((data) => {
-        // prodModal.innerHTML = ''
+        prodModalSlider.innerHTML = ''
+        prodModalInfo.innerHTML = ''
+        prodModalDescr.textContent = ''
+        prodModalChars.innerHTML = ''
 
         for (let dataItem of data) {
             if (dataItem.id == id) {
-                
-            }
+                const slides = dataItem.gallery.map(image => {
+                    return `
+                        <div class='product__slide'>
+                            <img src="${image}" alt="">
+                        </div>
+                    `
+                })
+                const preview = dataItem.gallery.map((image, idx) => {
+                    return `
+                        <div class="modal-preview__item data-slide="${idx}">
+                            <img src="${image}" alt="">
+                        </div>
+                    `
+                })
+                const sizes = dataItem.sizes.map(size => {
+                    return `
+                    <li class="modal-sizes__item"><button class="modal-sizes__btn">${size}</button></li>
+                    `
+                })
+
+                prodModalSlider.innerHTML = slides.join('')
+                prodModalSlider.nextElementSibling.innerHTML = preview.join('')
+                slideStart()
+
+                prodModalInfo.innerHTML = `
+                <h3 class="modal-info__title">${dataItem.title}</h3>
+                <div class="modal-info__rate">
+                    <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M7.04894 0.92705C7.3483 0.00573924 8.6517 0.00573965 8.95106 0.92705L10.0206 4.21885C10.1545 4.63087 10.5385 4.90983 10.9717 4.90983H14.4329C15.4016 4.90983 15.8044 6.14945 15.0207 6.71885L12.2205 8.75329C11.87 9.00793 11.7234 9.4593 11.8572 9.87132L12.9268 13.1631C13.2261 14.0844 12.1717 14.8506 11.388 14.2812L8.58779 12.2467C8.2373 11.9921 7.7627 11.9921 7.41221 12.2467L4.61204 14.2812C3.82833 14.8506 2.77385 14.0844 3.0732 13.1631L4.14277 9.87132C4.27665 9.4593 4.12999 9.00793 3.7795 8.75329L0.979333 6.71885C0.195619 6.14945 0.598395 4.90983 1.56712 4.90983H5.02832C5.46154 4.90983 5.8455 4.63087 5.97937 4.21885L7.04894 0.92705Z" fill="#F14F4F"/>
+                    </svg>
+                    <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M7.04894 0.92705C7.3483 0.00573924 8.6517 0.00573965 8.95106 0.92705L10.0206 4.21885C10.1545 4.63087 10.5385 4.90983 10.9717 4.90983H14.4329C15.4016 4.90983 15.8044 6.14945 15.0207 6.71885L12.2205 8.75329C11.87 9.00793 11.7234 9.4593 11.8572 9.87132L12.9268 13.1631C13.2261 14.0844 12.1717 14.8506 11.388 14.2812L8.58779 12.2467C8.2373 11.9921 7.7627 11.9921 7.41221 12.2467L4.61204 14.2812C3.82833 14.8506 2.77385 14.0844 3.0732 13.1631L4.14277 9.87132C4.27665 9.4593 4.12999 9.00793 3.7795 8.75329L0.979333 6.71885C0.195619 6.14945 0.598395 4.90983 1.56712 4.90983H5.02832C5.46154 4.90983 5.8455 4.63087 5.97937 4.21885L7.04894 0.92705Z" fill="#F14F4F"/>
+                    </svg>
+                    <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M7.04894 0.92705C7.3483 0.00573924 8.6517 0.00573965 8.95106 0.92705L10.0206 4.21885C10.1545 4.63087 10.5385 4.90983 10.9717 4.90983H14.4329C15.4016 4.90983 15.8044 6.14945 15.0207 6.71885L12.2205 8.75329C11.87 9.00793 11.7234 9.4593 11.8572 9.87132L12.9268 13.1631C13.2261 14.0844 12.1717 14.8506 11.388 14.2812L8.58779 12.2467C8.2373 11.9921 7.7627 11.9921 7.41221 12.2467L4.61204 14.2812C3.82833 14.8506 2.77385 14.0844 3.0732 13.1631L4.14277 9.87132C4.27665 9.4593 4.12999 9.00793 3.7795 8.75329L0.979333 6.71885C0.195619 6.14945 0.598395 4.90983 1.56712 4.90983H5.02832C5.46154 4.90983 5.8455 4.63087 5.97937 4.21885L7.04894 0.92705Z" fill="#F14F4F"/>
+                    </svg>
+                    <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M7.04894 0.92705C7.3483 0.00573924 8.6517 0.00573965 8.95106 0.92705L10.0206 4.21885C10.1545 4.63087 10.5385 4.90983 10.9717 4.90983H14.4329C15.4016 4.90983 15.8044 6.14945 15.0207 6.71885L12.2205 8.75329C11.87 9.00793 11.7234 9.4593 11.8572 9.87132L12.9268 13.1631C13.2261 14.0844 12.1717 14.8506 11.388 14.2812L8.58779 12.2467C8.2373 11.9921 7.7627 11.9921 7.41221 12.2467L4.61204 14.2812C3.82833 14.8506 2.77385 14.0844 3.0732 13.1631L4.14277 9.87132C4.27665 9.4593 4.12999 9.00793 3.7795 8.75329L0.979333 6.71885C0.195619 6.14945 0.598395 4.90983 1.56712 4.90983H5.02832C5.46154 4.90983 5.8455 4.63087 5.97937 4.21885L7.04894 0.92705Z" fill="#F14F4F"/>
+                    </svg>
+                    <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M7.04894 0.92705C7.3483 0.00573924 8.6517 0.00573965 8.95106 0.92705L10.0206 4.21885C10.1545 4.63087 10.5385 4.90983 10.9717 4.90983H14.4329C15.4016 4.90983 15.8044 6.14945 15.0207 6.71885L12.2205 8.75329C11.87 9.00793 11.7234 9.4593 11.8572 9.87132L12.9268 13.1631C13.2261 14.0844 12.1717 14.8506 11.388 14.2812L8.58779 12.2467C8.2373 11.9921 7.7627 11.9921 7.41221 12.2467L4.61204 14.2812C3.82833 14.8506 2.77385 14.0844 3.0732 13.1631L4.14277 9.87132C4.27665 9.4593 4.12999 9.00793 3.7795 8.75329L0.979333 6.71885C0.195619 6.14945 0.598395 4.90983 1.56712 4.90983H5.02832C5.46154 4.90983 5.8455 4.63087 5.97937 4.21885L7.04894 0.92705Z" fill="#F14F4F"/>
+                    </svg>
+                </div>
+                <div class="modal-info__sizes">
+                    <span class="modal-info__subtitle">Выберите размер</span>
+                    <ul class="modal-info__sizes-list modal-sizes">
+                        ${sizes.join('')}
+                    </ul>
+                </div>
+                <div class="modal-info__price">
+                    <span class="modal-info__current-price">${dataItem.price} ₽</span>
+                    <span class="modal-info__old-price">${dataItem.oldPrice ? dataItem.oldPrice + ' ₽' : ''}</span>
+                </div>
+                `
+
+                prodModalDescr.textContent = dataItem.description
+
+                let charsItems = ''
+                Object.keys(dataItem.chars).forEach(function eachKey(key) {
+                    charsItems += `<p class="prod-bottom__descr prod-chars__item">${key}: ${dataItem.chars[key]}</p>`
+                })
+                prodModalChars.innerHTML = charsItems
+            }   
         }
     })
 }
 
+
 const setModalProduct = () => {
-    document.querySelectorAll('.product__btn').forEach(el => {
+    document.querySelectorAll('.product__btn-view').forEach(el => {
         el.addEventListener('click', () => {
-            // modal.classList.add('modal--visable')
+            modal.classList.add('modal--visable')
             modalOverlay.classList.add('modal__overlay--visable')
             modalContent.classList.add('modal__content--visable')
             // document.body.style.overflow = 'hidden'
@@ -72,7 +141,7 @@ if (catalogList) {
                                 <div class="product__image">
                                     <img src="${item.mainImage}" alt="${item.title}">
                                     <div class="product__btns">
-                                        <button data-id="${item.id}" class="product__btn" aria-label="Показать информацию о товаре">
+                                        <button data-id="${item.id}" class="product__btn product__btn-view" aria-label="Показать информацию о товаре">
                                             <svg width="23" height="18" viewBox="0 0 26 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M16.9518 10.0664C16.9518 12.2489 15.1818 14.0176 12.9993 14.0176C10.8168 14.0176 9.0481 12.2489 9.0481 10.0664C9.0481 7.88264 10.8168 6.11389 12.9993 6.11389C15.1818 6.11389 16.9518 7.88264 16.9518 10.0664Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M12.9975 19.1936C17.7575 19.1936 22.1113 15.7711 24.5625 10.0661C22.1113 4.3611 17.7575 0.938599 12.9975 0.938599H13.0025C8.2425 0.938599 3.88875 4.3611 1.4375 10.0661C3.88875 15.7711 8.2425 19.1936 13.0025 19.1936H12.9975Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
