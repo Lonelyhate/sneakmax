f4a5c49bb42e7dcb6b15928f8b9ce4620157c67c
Nikita Fedorov <nikitafedorov666@icloud.com>
2021-11-05 01:12:04 +0300

    creating modal order

--- js/products.js
+++ js/products.js
@@ -10,6 +10,8 @@ const prodModalSlider = prodModal.querySelector('.product-slider__wrapper')
 const prodModalInfo = prodModal.querySelector('.modal-info__wrapper')
 const prodModalDescr = prodModal.querySelector('.modal-prod-descr')
 const prodModalChars = prodModal.querySelector('.prod-chars')
+const modalCart = document.querySelector('.modal__buy')
+const modalCartOverlay = document.querySelector('.modal__overlay-buy')
 
 const normalPrice = (str) => {
 	return String(str).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
@@ -120,6 +122,23 @@ const setModalProduct = () => {
     })
 }
 
+const cartModalProduct = () => {
+    document.querySelectorAll('.product__btn-cart').forEach(el => {
+        el.addEventListener('click', e => {
+            modalCart.classList.add('modal__buy--visable')
+            modalCartOverlay.classList.add('modal__overlay-buy--visable')
+            document.querySelector('.modal__content-buy').classList.add('modal__content-buy--visable')
+        })
+    })
+    modalCartOverlay.addEventListener('click', e => {
+        if (e.target == modalCartOverlay) {
+            modalCart.classList.remove('modal__buy--visable')
+            modalCartOverlay.classList.remove('modal__overlay-buy--visable')
+            document.querySelector('.modal__content-buy').classList.remove('modal__content-buy--visable')
+        }
+    })
+}
+
 if (catalogList) {
     const loadProducts = (quantity = 6) => {
         fetch('../resources/data/data.json')
@@ -147,7 +166,7 @@ if (catalogList) {
                                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M12.9975 19.1936C17.7575 19.1936 22.1113 15.7711 24.5625 10.0661C22.1113 4.3611 17.7575 0.938599 12.9975 0.938599H13.0025C8.2425 0.938599 3.88875 4.3611 1.4375 10.0661C3.88875 15.7711 8.2425 19.1936 13.0025 19.1936H12.9975Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                                             </svg>
                                         </button>
-                                        <button class="product__btn" aria-label="Добавить товар в корзину">
+                                        <button class="product__btn product__btn-cart" aria-label="Добавить товар в корзину">
                                             <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M20 6.57893H16.6038L13.0359 0.319309C12.8589 0.0083658 12.475 -0.093156 12.1784 0.0938088C11.8823 0.280773 11.7865 0.684867 11.9641 0.996475L15.1461 6.57893H4.85388L8.03587 0.996431C8.21348 0.684823 8.11767 0.280729 7.82164 0.0937645C7.52439 -0.0932003 7.14173 0.00832153 6.96411 0.319265L3.39617 6.57888H0V7.89468H1.35651L2.94432 16.8103C3.11033 17.7438 3.88547 18.421 4.78761 18.421H15.2124C16.1145 18.421 16.8896 17.7438 17.055 16.811L18.6434 7.89468H20C20 7.89468 20 6.57893 20 6.57893ZM15.8264 16.5688C15.7715 16.8797 15.5133 17.1053 15.2124 17.1053H4.78761C4.4867 17.1053 4.22854 16.8798 4.173 16.5681L2.62789 7.89468H17.3721L15.8264 16.5688Z" fill="white"/>
                                             </svg>
@@ -164,6 +183,7 @@ if (catalogList) {
         })
         .then(() => {
             setModalProduct()
+            cartModalProduct()
         })
     }
     loadProducts(prodQuantity)
